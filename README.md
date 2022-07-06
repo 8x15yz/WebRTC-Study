5195ff649063cb3ea5005d4d6fe063be283796b6
8x15yz <8x15yz@gmail.com>
2022-07-06 21:07:56 +0900

    WebRTC with WebSocket

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Noom
 
 Zoom clone using NodeJS, WebRTC and Websockents.
+
+## 0 WebRTC 만들기 셋팅
+
+## 1 WebSocket 사용
+
+## 2 socket.io 사용하기 (websocket framework)
--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@
 import express from "express";
 
 import http from "http";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import SocketIO from "socket.io"
 
 const app = express(); 
 
@@ -16,30 +17,44 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose(message) {
+
+
+
+
+/* Websocket 사용한 Web RTC 로직
+
+function onSocketClose() {
     console.log("Disconnected from the Browser ❌");
-}
-
-function onSocketMessage(message) {
-    console.log(message.toString('utf8'));
 }
 
 const sockets = [];
 
 wss.on("connection", (socket) => { // 연결했을 때 사용하는 함수 => 지금 연결을 기다리는 중인거임
     sockets.push(socket);
+    socket["nickname"] = "Anonymous";
+
     console.log("Connected to Browser ✅");
     socket.on("close", () => onSocketClose);
-    socket.on("message", (message) => {
-        // socket.send(message.toString('utf8'));
-        sockets.forEach(aSocket => aSocket.send(message.toString('utf8')));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg); // JSON을 파싱해줘야될 필요가 있다 (객체로 들어가야되기 때문)
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+
     });
     // https://velog.io/@plate0113/JS-Buffer-68-65-6c-6c-6f-20-66-72-6f-6d-20-74-68-65-20-62-72-6f-77-73-65-72-73-%ED%98%84%EC%83%81-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95
     // message.toString('utf8') 문자열 깨져서 나타나는 현상 해결
 }); 
 
+*/
 
-server.listen(3000, handleListen);
+
+httpServer.listen(3000, handleListen);
